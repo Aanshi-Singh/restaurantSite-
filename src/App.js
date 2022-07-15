698c50ea28cdc14d914d13424ba584e76fdced3b
Aanshi <aanshisingh04022002@gmail.com>
2022-07-15 13:57:18 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+const App=()=>{
+  return (
+    <>
+    <MyName/>
+      <h1>hello world {3+3}</h1>
+      
+    </>
+  )
+};
+
+const MyName =()=>{
+  return <h1>aanshi</h1>
+};
+
+
+
+export default App;
